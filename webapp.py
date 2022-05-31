6740a2702356725b40ad611dbbf05a1681c21a32
christopher graner <chrisgraner1@gmail.com>
2022-05-31 09:46:10 -0700

    wd

--- webapp.py
+++ webapp.py
@@ -13,10 +13,10 @@ import sys
 
 app = Flask(__name__)
 
-app.debug = True #Change this to False for production
+app.debug = False #Change this to False for production
 #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #Remove once done debugging
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
@@ -56,7 +56,7 @@ def home():
 #redirect to GitHub's OAuth page and confirm callback URL
 @app.route('/login')
 def login():
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 @app.route('/logout')
 def logout():
